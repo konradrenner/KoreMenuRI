906b626e3a4ea7a3dd9f3312bb6529acd33e884f
Konrad Renner <koni.renner@gmail.com>
2013-08-13 19:37:19 +0200

    first implementations

---
+++ src/main/java/org/kore/menu/ri/NamespaceImpl.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kore.menu.ri;
+
+import org.kore.menu.api.Namespace;
+
+/**
+ *
+ * @author Konrad Renner
+ */
+public class NamespaceImpl implements Namespace {
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
---
+++ src/test/java/org/kore/menu/ri/SecurityUIDImpl.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kore.menu.ri;
+
+import org.kore.menu.api.security.SecurityUID;
+
+/**
+ *
+ * @author Konrad Renner
+ */
+public class SecurityUIDImpl implements SecurityUID {
+
+    @Override
+    public String getIdentifierString() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
